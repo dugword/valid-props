dd7b5f1972391ce069faf4ab53493efa6658b00e
Douglas L. Jenkins <dugword@gmail.com>
2014-11-20 06:53:53 -0800

    required property checking

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('TODO: Not yet implemented');
+'use strict';
+
+var self = module.exports;
+
+var verbose = true;
+
+self.validate = function (params, schema) {
+
+	// Check that every required property exists
+	var missingProperties = [];
+
+	Object.keys(schema).forEach(function (requiredProperty) {
+		if (!params[requiredProperty]) return missingProperties.push(requiredProperty);
+	});
+
+	if (missingProperties.length) {
+		verbose && console.log('Missing properties:' + missingProperties.join(', '));
+		return false;
+	}
+
+	// Check that every request is of the required type
+	var incorrectTypes = [];
+
+	Object.keys(schema).forEach(function (requiredProperty) {
+		// if(typeof params[requiredProperty] !== 
+	})
+};
