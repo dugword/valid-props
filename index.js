81f913ae9458b2c6fa237b91bf9070c8dab9c806
Douglas L. Jenkins <jenkinsd@ochin.org>
2014-11-21 12:36:36 -0800

    Checking types

--- index.js
+++ index.js
@@ -4,52 +4,58 @@ var self = module.exports;
 
 var verbose = true;
 
+self.setVerbose = function (flag) {
+    if (flag) verbose = true;
+};
+
 self.validate = function (params, schema) {
 
-	// Check that every required property exists
-	var missingProperties = [];
+    // Check that every required property exists
+    var missingProperties = [];
 
-	Object.keys(schema).forEach(function (requiredProperty) {
-		if (!params[requiredProperty]) {
-			return missingProperties.push(requiredProperty);
-		}
-	});
+    Object.keys(schema).forEach(function (requiredProperty) {
+        if (!params[requiredProperty]) {
+            return missingProperties.push(requiredProperty);
+        }
+    });
 
-	if (missingProperties.length) {
-		verbose && console.log('Missing properties:' + missingProperties.join(', '));
-		return false;
-	}
+    if (missingProperties.length) {
+        if (verbose) console.error('Missing properties:' + missingProperties.join(', '));
+        return false;
+    }
 
-	// Check that every request is of the required type
-	var incorrectTypes = [];
+    // Check that every request is of the required type
+    var incorrectTypes = [];
 
-	Object.keys(schema).forEach(function (requiredProperty) {
-		var type = schema[requiredProperty];
+    Object.keys(schema).forEach(function (requiredProperty) {
+        var type = schema[requiredProperty],
+            value = params[requiredProperty];
 
-		if (type === 'date') {
+        if (type === 'date' && new Date(type).toString() === 'Invalid Date') {
+            return incorrectTypes.push(requiredProperty);
+        }
 
-		}	
+        else if (type === 'array' && !Array.isArray(type)) {
 
-		else if (type === 'array') {
+        }
 
-		}
+        else if (typeof params[requiredProperty] !== schema[requiredProperty]) {
+            return incorrectTypes.push(requiredProperty);
+        }
+    });
 
-		else if (type === ) {
+    if (incorrectTypes.length) {
+        if (verbose) console.error('Incorrect types:' + incorrectTypes.join(', '));
+        return false;
+    }
 
-		}
+    // Strip params of unspecified properties
 
-		else if(typeof params[requiredProperty] !== schema[requiredProperty]) {
-			return incorrectType.push((requiredProperty);
-		}
-	});
+    var cleanParams = {};
 
-	// Strip params of unspecified properties
-	
-	cleanParams = {};
+    Object.keys(schema).forEach(function (requiredProperty) {
+        cleanParams[requiredProperty] = params[requiredProperty];
+    });
 
-	Object.keys(schema).forEach(function (requiredProperty) {
-		cleanParams[requiredProperty] = params[requiredProperty];
-	});
-
-	return cleanParams;
+    return cleanParams;
 };
