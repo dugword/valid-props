f255bb49a220ca5884139eb1ee61bae90d5f3477
Douglas L. Jenkins <jenkinsd@ochin.org>
2015-02-08 13:35:31 -0800

    more tests

--- test/test.js
+++ test/test.js
@@ -4,19 +4,19 @@ var assert = require('assert'),
     props = require('../'),
     expect = require('chai').expect;
 
-describe('valid-props', function () {
+describe('Confirm valid object', function () {
     var raw = {
-        myString: 'Puppy',
+        myString: 'puppy',
         myNumber: 42,
         myArray: ['who', 'what', 'where', 'when', 'why'],
         myTypedArray: [24, 13, 30, 1337],
         myObject: {
             spam: 'eggs'
         },
-        myDate: new Date()
+        myDate: new Date(),
+        myTrue: true,
+        myFalse: false
     };
-
-    // raw.myNumber = 'puppy';
 
     var valid = props.validate(raw, {
         myString: 'string',
@@ -24,7 +24,9 @@ describe('valid-props', function () {
         myArray: 'array',
         myTypedArray: '[number]',
         myObject: 'object',
-        myDate: 'date'
+        myDate: 'date',
+        myTrue: 'boolean',
+        myFalse: 'boolean'
     });
 
     it('valid should not be undefined', function () {
@@ -43,11 +45,23 @@ describe('valid-props', function () {
         expect(valid.myArray).to.be.an('array');
     });
 
+    it('valid.myTypedArray should be an array of numbers', function () {
+        expect(valid.myTypedArray).to.have.members([24, 13, 30, 1337]);
+    });
+
     it('valid.myObject should be an object', function () {
         expect(valid.myObject).to.be.an('object');
     });
 
     it('valid.myDate should be a date', function () {
         expect(valid.myDate).to.be.an.instanceof(Date);
+    });
+
+    it('valid myTrue should be true', function () {
+        expect(valid.myTrue).to.be.true();
+    });
+
+    it('valid myFalse should be false', function () {
+        expect(valid.myFalse).to.be.false();
     });
 });
