fcb39c61eb11201862865b6f8aa3ff5087f2626a
sean <ohollarens@ochin.org>
2015-02-12 10:13:12 -0800

    Added support for boolean validation and changed optionalProperty existence check to not fail on booleans which exist but are false

--- index.js
+++ index.js
@@ -7,112 +7,119 @@ var verbose = true;
 // Private Methods
 
 var checkForMissingProperties = function (params, schema) {
-    var missingProperties = [];
+  var missingProperties = [];
 
-    Object.keys(schema).forEach(function (requiredProperty) {
-        if (!params[requiredProperty]) {
-            return missingProperties.push(requiredProperty);
-        }
-    });
+  Object.keys(schema).forEach(function (requiredProperty) {
+    if (!params[requiredProperty]) {
+      return missingProperties.push(requiredProperty);
+    }
+  });
 
-    return missingProperties;
+  return missingProperties;
 };
 
 var checkPropertiesTypes = function (params, schema) {
-    var incorrectTypes = [];
+  var incorrectTypes = [];
 
-    Object.keys(schema).forEach(function (requiredProperty) {
-        var requiredType = schema[requiredProperty],
-            value = params[requiredProperty];
+  Object.keys(schema).forEach(function (requiredProperty) {
+    var requiredType = schema[requiredProperty],
+    value = params[requiredProperty];
 
-        // Don't check types on missing properties
-        if (!value) return;
+    // Don't check types on missing properties
+    if (!value) return;
 
-        if (requiredType === 'date') {
-            value = new Date(value);
-            if (value.toString() === 'Invalid Date') {
-                return incorrectTypes.push(requiredProperty + ' not a valid date');
-            }
-            params[requiredProperty] = value;
-        }
+    if (requiredType === 'date') {
+      value = new Date(value);
+      if (value.toString() === 'Invalid Date') {
+        return incorrectTypes.push(requiredProperty + ' not a valid date');
+      }
+      params[requiredProperty] = value;
+    }
 
-        else if (requiredType === 'array') {
-            if (!Array.isArray(value)) {
-                return incorrectTypes.push(requiredProperty + ' not an array');
-            }
-        }
+    else if (requiredType === 'array') {
+      if (!Array.isArray(value)) {
+        return incorrectTypes.push(requiredProperty + ' not an array');
+      }
+    }
 
-        else if (requiredType === 'number') {
-            value = Number(value);
-            if (isNaN(value)) {
-                return incorrectTypes.push(requiredProperty + ' not a number');
-            }
-            params[requiredProperty] = value;
-        }
+    else if (requiredType === 'number') {
+      value = Number(value);
+      if (isNaN(value)) {
+        return incorrectTypes.push(requiredProperty + ' not a number');
+      }
+      params[requiredProperty] = value;
+    }
 
-        else {
-            if (typeof params[requiredProperty] !== schema[requiredProperty]) {
-                return incorrectTypes.push(requiredProperty + ' not a ' + requiredType);
-            }
-        }
+    else if (requiredType === 'boolean') {
+      if (value.toLowerCase() === 'false') {
+        value = false;
+      }
+      else value = Boolean(value);
+      params[requiredProperty] = value;
+    }
 
-    });
+    else {
+      if (typeof params[requiredProperty] !== schema[requiredProperty]) {
+        return incorrectTypes.push(requiredProperty + ' not a ' + requiredType);
+      }
+    }
 
-    return incorrectTypes;
+  });
+
+  return incorrectTypes;
 };
 
 // Public Methods
 
 self.setVerbose = function (flag) {
-    if (flag) verbose = true;
+  if (flag) verbose = true;
 };
 
 self.validate = function (params, schema) {
 
-    var optional = schema.optional || {};
-    delete schema.optional;
+  var optional = schema.optional || {};
+  delete schema.optional;
 
 
-    // Check that every required property exists
-    var missingProperties = checkForMissingProperties(params, schema);
+  // Check that every required property exists
+  var missingProperties = checkForMissingProperties(params, schema);
 
-    if (missingProperties.length) {
-        if (verbose) console.error('Missing properties:',  missingProperties.join(', '));
-        return false;
+  if (missingProperties.length) {
+    if (verbose) console.error('Missing properties:', missingProperties.join(', '));
+    return false;
+  }
+
+  // Check that every required property is of the required type
+  var incorrectTypes = checkPropertiesTypes(params, schema);
+
+  if (incorrectTypes.length) {
+    if (verbose) console.error('Incorrect required type:', incorrectTypes.join(', '));
+    return false;
+  }
+
+
+  // Check that every optional request is of the required type
+  incorrectTypes = checkPropertiesTypes(params, optional);
+
+  if (incorrectTypes.length) {
+    if (verbose) console.error('Incorrect optional type:', incorrectTypes.join(', '));
+    return false;
+  }
+
+
+  // Strip params of unspecified properties
+
+  var cleanParams = {};
+
+  Object.keys(schema).forEach(function (requiredProperty) {
+    cleanParams[requiredProperty] = params[requiredProperty];
+  });
+
+  Object.keys(optional).forEach(function (optionalProperty) {
+    if (params[optionalProperty] !== undefined) {
+      cleanParams[optionalProperty] = params[optionalProperty];
     }
+  });
 
-    // Check that every required property is of the required type
-    var incorrectTypes = checkPropertiesTypes(params, schema);
-
-    if (incorrectTypes.length) {
-        if (verbose) console.error('Incorrect required type:', incorrectTypes.join(', '));
-        return false;
-    }
-
-
-    // Check that every optional request is of the required type
-    incorrectTypes = checkPropertiesTypes(params, optional);
-
-    if (incorrectTypes.length) {
-        if (verbose) console.error('Incorrect optional type:', incorrectTypes.join(', '));
-        return false;
-    }
-
-
-    // Strip params of unspecified properties
-
-    var cleanParams = {};
-
-    Object.keys(schema).forEach(function (requiredProperty) {
-        cleanParams[requiredProperty] = params[requiredProperty];
-    });
-
-    Object.keys(optional).forEach(function (optionalProperty) {
-        if (params[optionalProperty]) {
-            cleanParams[optionalProperty] = params[optionalProperty];
-        }
-    });
-
-
-    return cleanParams;
+  return cleanParams;
 };
