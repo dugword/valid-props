219e5e6940e1877115bd72169dc14e4fd24d2162
Douglas L. Jenkins <jenkinsd@ochin.org>
2015-11-18 16:54:19 -0800

    adde invalid type test file

---
+++ test/invalid-type.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var assert = require('assert'),
+    props = require('../'),
+    expect = require('chai').expect,
+    raw = require('./raw');
+
+describe('Invalid types should throw error', function () {
+
+    it('Invalid type should throw', function () {
+        try {
+            var valid = props.validate(raw.all, {
+                myString: 'invalid-type',
+            });
+        }
+        catch (err) {
+            expect(err.message).to.equal('Invalid type in schema: invalid-type');
+        }
+    });
+
+    it('Invalid double api optional should throw error', function () {
+        try {
+            var validTwo = props.validate(raw.all, {
+                myString: 'string',
+            }, {
+                myNumber: 'number?'
+            });
+        }
+        catch (err) {
+            expect(err.message).to.equal('Invalid type in schema: number?');
+        }
+    });
+
+});
