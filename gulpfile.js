09f5d8f600d62830530d7616162846a903ed84c5
Douglas L. Jenkins <dugword@gmail.com>
2015-11-26 12:10:33 -0800

    fixed and updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,9 @@ const gulp = require('gulp'),
     babel = require('gulp-babel');
 
 gulp.task('default', () => {
-    console.error('Not yet set up');
-
     return gulp.src('src/index.js')
         .pipe(babel({
             presets: ['es2015']
         }))
-        .pipe(gulp.dest('index.js'));
+        .pipe(gulp.dest('./'));
 });
